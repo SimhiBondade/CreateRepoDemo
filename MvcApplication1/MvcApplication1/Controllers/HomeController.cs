f25f997cdff0cfa726bd3d78f962deb2af93e53d
Shoma <shoma.gujjar@milkround.com>
2014-02-11 14:11:16 +0000

    Committing changes to the branch.

--- MvcApplication1/MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.Jai Sai Ram";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.Jai Sai Ram Sai Nath tere hazarro haath";
 
             return View();
         }
